a697140f499f0a534eb930992e96f7744bd219d6
LorenzoPiombini <69109614+LorenzoPiombini@users.noreply.github.com>
2020-09-26 02:04:02 -0400

    Initial Commit

---
+++ CoolCalc3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoolCalc3
+//
+//  Created by Lorenzo piombini on 9/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
